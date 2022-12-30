718d59cb34eda397b2b0ad795407201d22a4e53a
Instructor <61094918+Instructor-Team8@users.noreply.github.com>
2022-12-30 22:44:30 +0300

    Add files via upload

---
+++ project/src/index.php
@@ -0,0 +1,6 @@
+<?php
+// index.php window.location.replace('https://example.com');
+//header("Location: http://localhost/index1.php");
+echo $_POST['name'];
+echo "Привет!Через 5 секунд вас выкинет на Рамблер))";
+?>
